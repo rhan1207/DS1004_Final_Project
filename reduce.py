5983e40515338d26d21cf61e0f3f9df4826ae097
rhan1207 <hanrj1207@yahoo.com>
2016-04-24 16:24:28 -0400

    first_test

---
+++ reduce.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import sys
+
+current_key = None
+count = 0
+
+# input comes from STDIN (stream data that goes to the program)
+for line in sys.stdin:
+    key, val = line.strip().split("\t", 1)
+    if key != current_key:
+        if current_key != None:
+            print "%s\t%d" % (current_key, count)
+        count = 0
+        current_key = key
+
+    count = count + 1
+
+print "%s\t%d" % (current_key, count)
