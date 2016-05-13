fdc0e493331ebf6559ff4c64310d789b9fc3dc3e
unknown <ryan.weber@ryanweber.andiamo.local>
2016-05-13 13:23:04 -0400

    years

--- construction/map.py
+++ construction/map.py
@@ -7,8 +7,12 @@ import numpy as np
 
 for line in sys.stdin:
     l = line.strip().split(",")
-    if l[0] != "Source":
+    if l[0] != "Source" and len(l)>13 and len(str(l[13])) ==5:
         zip = str(l[13])
-        print "%s,\t%d" % (key,1)
-
-
+        date = l[6]
+        try:
+			int(zip)
+			print "%s\t%s" % (zip,(str(int(u'2009' in date)) + "," + str(int(u'2010' in date)) + "," + str(int(u'2011' in date)) + "," + str(int(u'2012' in date)) + "," + str(int(u'2013' in date)) + "," + str(int(u'2014' in date))))
+        except:
+            zip = 1
+        #print "%s\t%s" % (zip,str(int(u'2009' in date)) + "," + str(int(u'2010' in date)) + "," + str(int(u'2011' in date)) + "," + str(int(u'2012' in date)) + "," + str(int(u'2013' in date)) + "," + str(int(u'2014' in date)))
--- construction/reduce.py
+++ construction/reduce.py
@@ -3,17 +3,23 @@
 import sys
 
 current_key = None
-count = 0
+counts = [0,0,0,0,0,0]
 
 # input comes from STDIN (stream data that goes to the program)
 for line in sys.stdin:
     key, val = line.strip().split("\t", 1)
+    yearCounts = val.strip().split(",")
     if key != current_key:
         if current_key != None:
-            print "%s\t%d" % (current_key, count)
-        count = 0
+            print "%s,%d,%d,%d,%d,%d,%d" % (current_key, counts[0], counts[1], counts[2], counts[3], counts[4], counts[5])
+        counts = [0,0,0,0,0,0]
         current_key = key
 
-    count = count + 1
+    counts[0] = counts[0] + int(yearCounts[0])
+    counts[1] = counts[1] + int(yearCounts[1])
+    counts[2] = counts[2] + int(yearCounts[2])
+    counts[3] = counts[3] + int(yearCounts[3])
+    counts[4] = counts[4] + int(yearCounts[4])
+    counts[5] = counts[5] + int(yearCounts[5])
 
-print "%s\t%d" % (current_key, count)
+print "%s,%d,%d,%d,%d,%d,%d" % (current_key, counts[0], counts[1], counts[2], counts[3], counts[4], counts[5])
